972db590eabb1662326eae97a315b49cc8ebcc62
Benson Waweru <bensonkogi20@gmail.com>
2025-11-03 22:21:45 +0300

    fix/minor-bug

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
   const [currentPage, setCurrentPage] = useState("dashboard");
   return (
     <div className='min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-900 dark:to-slate-900 transition-all duration-500'>
-
       <div className='flex h-screen overflow-hidden'>
         <Sidebar
           collapsed={sideBarCollapsed}
@@ -17,6 +16,7 @@ const App = () => {
           currentPage={currentPage}
           onPageChange={setCurrentPage}
         />
+
         <div className='flex-1 flex flex-col overflow-hidden'>
           <Header
             sideBarCollapsed={sideBarCollapsed}
@@ -28,8 +28,8 @@ const App = () => {
               {currentPage === "dashboard" && <Dashboard/> }
             </div>
           </main>
-
         </div>
+
       </div>
 
     </div>
